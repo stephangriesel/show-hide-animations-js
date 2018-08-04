ca72c21000f56e5c84aeb5ed8c86acec0dfe4bd4
Stephan Griesel <sgriesel@gmail.com>
2018-08-04 09:10:59 +0200

    add styling

---
+++ script.js
@@ -0,0 +1,11 @@
+var aniblock = document.querySelector("#aniblock");
+document.addEventListener('click', function(event) {
+    /*if (event.target.matches('.show')) {
+        aniblock.removeAttribute('hidden');
+        aniblock.classList.add('fadeInDown');
+    }
+    if (event.target.matches('.hide')) {
+        aniblock.setAttribute('hidden', 'true');
+        aniblock.classList.remove('fadeInDown');
+    }*/
+}, false);
